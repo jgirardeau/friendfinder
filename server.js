1ccdad5b28f96c31f63f8f12a6968771e79b5403
Jim Girardeau <jwgirardeau@gmail.com>
2019-01-19 20:43:29 -0600

    forms working

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -12,33 +11,9 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//require("./routes/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/survey.html"));
-});
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/home.html"));
-  });
-
-  app.get("/data", function (req, res) {
-    decode_form();
-});
-  
-  // app.get("/add", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "add.html"));
-  // });
-  
-  // Displays all characters
-//   app.get("/public/survey", function (req, res) {
-//       return res.json(current_survey);
-//   });
-  
-//   // Displays a single character, or returns false
-//   app.get("/api/waitlist", function (req, res) {
-//       return res.json(wait_list);
-  
-//   });
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
-  });
+});
